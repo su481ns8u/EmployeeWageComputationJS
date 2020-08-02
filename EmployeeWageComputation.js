efdc1a50810ac8a3dfe4147a6a2f28591f2a5bcd
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-02 17:31:14 +0530

    ADDED Code to calculate employee wage for full day

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,9 +1,16 @@
-console.log("Welcome to Employee Wage Computation")
+console.log("Welcome to Employee Wage Computation");
 
+let EMP_RATE_PER_HR = 20;
+let FULL_DAY_HR = 8;
+
+let empWorkHrs = 0;
+let empWage = 0;
 let isPresent = 1;
 let empAttendanceCheck = Math.floor(Math.random() * 10) % 2;
 
-if (empAttendanceCheck == isPresent)
-    console.log("Employee is present")
-else
-    console.log("Employee is absent")
+if (empAttendanceCheck == isPresent) {
+  console.log("Employee is present");
+  empWage = FULL_DAY_HR * EMP_RATE_PER_HR;
+} else console.log("Employee is absent");
+
+console.log("Employee wage is " + empWage);
