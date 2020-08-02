933978482df3eb576e92bab038a8b31f71e2e05a
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-02 18:58:39 +0530

    REFACTORED Code to create separate function to get emp work hrs for day

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -11,21 +11,23 @@ let totalEmpHrs = 0;
 let totalWage = 0;
 let workDays = 0;
 
-// for (let day = 0; day < TOTAL_DAYS_IN_MONTH; day++) {
-while (totalEmpHrs <= 100 && workDays <= TOTAL_DAYS_IN_MONTH) {
-  let empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
+function getEmpWorkHrsForDay(empAttendanceCheck) {
   switch (empAttendanceCheck) {
     case IS_FULL_TIME:
       console.log("Employee is present for full time");
-      totalEmpHrs += FULL_DAY_HR;
-      break;
+      return FULL_DAY_HR;
     case IS_PART_TIME:
       console.log("Employee is present for part time");
-      totalEmpHrs += PART_DAY_HR;
-      break;
+      return PART_DAY_HR;
     default:
       console.log("Employee is absent");
+      return 0;
   }
+}
+
+while (totalEmpHrs <= 100 && workDays <= TOTAL_DAYS_IN_MONTH) {
+  let empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
+  totalEmpHrs += getEmpWorkHrsForDay(empAttendanceCheck);
   workDays++;
 }
 
