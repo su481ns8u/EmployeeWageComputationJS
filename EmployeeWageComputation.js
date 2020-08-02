e9077502f2c767b16c5e1452bd463407ff476e18
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-02 18:03:16 +0530

    REFACTORED Code to replace if conditions with switch

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -9,12 +9,17 @@ let IS_PART_TIME = 2;
 let empWage = 0;
 let empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
 
-if (empAttendanceCheck == IS_FULL_TIME) {
-  console.log("Employee is present for full time");
-  empWage = FULL_DAY_HR * EMP_RATE_PER_HR;
-} else if (empAttendanceCheck == IS_PART_TIME) {
-  console.log("Employee is present for part time");
-  empWage = PART_DAY_HR * EMP_RATE_PER_HR;
-} else console.log("Employee is absent");
+switch (empAttendanceCheck) {
+  case IS_FULL_TIME:
+    console.log("Employee is present for full time");
+    empWage = FULL_DAY_HR * EMP_RATE_PER_HR;
+    break;
+  case IS_PART_TIME:
+    console.log("Employee is present for part time");
+    empWage = PART_DAY_HR * EMP_RATE_PER_HR;
+    break;
+  default:
+    console.log("Employee is absent");
+}
 
 console.log("Employee wage is " + empWage);
