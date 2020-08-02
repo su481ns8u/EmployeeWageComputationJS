428f8b49b629d0c4ba145e44b4394abfe13f4857
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-02 18:43:52 +0530

    REFACTORED Code to calculate wage till the given condition is satisfied

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -7,25 +7,27 @@ let IS_FULL_TIME = 1;
 let IS_PART_TIME = 2;
 let TOTAL_DAYS_IN_MONTH = 20;
 
-let empWage = 0;
+let totalEmpHrs = 0;
 let totalWage = 0;
+let workDays = 0;
 
-for (let day = 0; day < TOTAL_DAYS_IN_MONTH; day++) {
+// for (let day = 0; day < TOTAL_DAYS_IN_MONTH; day++) {
+while (totalEmpHrs <= 100 && workDays <= TOTAL_DAYS_IN_MONTH) {
   let empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
   switch (empAttendanceCheck) {
     case IS_FULL_TIME:
       console.log("Employee is present for full time");
-      empWage = FULL_DAY_HR * EMP_RATE_PER_HR;
+      totalEmpHrs += FULL_DAY_HR;
       break;
     case IS_PART_TIME:
       console.log("Employee is present for part time");
-      empWage = PART_DAY_HR * EMP_RATE_PER_HR;
+      totalEmpHrs += PART_DAY_HR;
       break;
     default:
       console.log("Employee is absent");
-      empWage = 0;
   }
-  totalWage += empWage;
+  workDays++;
 }
 
+totalWage = totalEmpHrs * EMP_RATE_PER_HR;
 console.log("Employee total wage for month is " + totalWage);
