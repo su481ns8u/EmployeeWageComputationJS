a8879c8ca6cfb4e9655ed402a46aeb516200c6be
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-02 17:49:37 +0530

    ADDED Code to calculate emp wage for part time

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -2,15 +2,19 @@ console.log("Welcome to Employee Wage Computation");
 
 let EMP_RATE_PER_HR = 20;
 let FULL_DAY_HR = 8;
+let PART_DAY_HR = 4;
+let IS_FULL_TIME = 1;
+let IS_PART_TIME = 2;
 
-let empWorkHrs = 0;
 let empWage = 0;
-let isPresent = 1;
-let empAttendanceCheck = Math.floor(Math.random() * 10) % 2;
+let empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
 
-if (empAttendanceCheck == isPresent) {
-  console.log("Employee is present");
+if (empAttendanceCheck == IS_FULL_TIME) {
+  console.log("Employee is present for full time");
   empWage = FULL_DAY_HR * EMP_RATE_PER_HR;
+} else if (empAttendanceCheck == IS_PART_TIME) {
+  console.log("Employee is present for part time");
+  empWage = PART_DAY_HR * EMP_RATE_PER_HR;
 } else console.log("Employee is absent");
 
 console.log("Employee wage is " + empWage);
